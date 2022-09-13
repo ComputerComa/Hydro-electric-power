dc63f262a8382457c272de9a0c76f51cb529afd0
ComputerComa <68386555+ComputerComa@users.noreply.github.com>
2022-09-13 00:06:31 -0500

    see changelog

--- HydroelectricPower_1.0.2/prototypes/item.lua
+++ HydroelectricPower_1.0.2/prototypes/item.lua
@@ -23,6 +23,28 @@ data:extend({
     },
     {
       type = "item",
+      name = "small-hydroelectric-turbine-old",
+      icon = "__base__/graphics/icons/steam-engine.png",
+      icon_size = 64,
+     -- flags = {"goes-to-quickbar"},
+      subgroup = "energy",
+      order = "d[steam-power]-h[steam-turbine]",
+      place_result = "small-hydroelectric-turbine-old",
+      stack_size = 10
+    },
+    {
+      type = "item",
+      name = "large-hydroelectric-turbine-old",
+      icon = "__base__/graphics/icons/steam-engine.png",
+      icon_size = 64,
+     -- flags = {"goes-to-quickbar"},
+      subgroup = "energy",
+      order = "d[steam-power]-h[steam-turbine]",
+      place_result = "large-hydroelectric-turbine-old",
+      stack_size = 10
+    },
+    {
+      type = "item",
       name = "high-speed-offshore-pump",
       icon = "__base__/graphics/icons/offshore-pump.png",
       icon_size = 64, icon_mipmaps = 4,
--- HydroelectricPower_1.0.2/prototypes/recipe.lua
+++ HydroelectricPower_1.0.2/prototypes/recipe.lua
@@ -17,6 +17,22 @@ data:extend({
       },
       {
         type = "recipe",
+        name = "small-hydroelectric-turbine-old",
+        enabled = true,
+        energy_required = 2,
+        ingredients = {{"iron-gear-wheel", 8}, {"pipe", 2}, {"iron-plate", 5}},
+        result = "small-hydroelectric-turbine-old"
+      },
+      {
+        type = "recipe",
+        name = "large-hydroelectric-turbine-old",
+        enabled = true,
+        energy_required = 4,
+        ingredients = {{"iron-gear-wheel", 16},  {"pipe", 4}, {"iron-plate", 10}},
+        result = "large-hydroelectric-turbine-old"
+      },
+      {
+        type = "recipe",
         name = "high-speed-offshore-pump",
         enabled = "false",
         ingredients = {
@@ -27,5 +43,40 @@ data:extend({
           {"offshore-pump",2}
         },
         result = "high-speed-offshore-pump"
+      },
+      {
+        type = "recipe",
+        name = "small-hydroelectric-turbine-old-to-new",
+        enabled = true,
+        energy_required = .5,
+        ingredients = {{"small-hydroelectric-turbine-old", 1}},
+        result = "small-hydroelectric-turbine"
+      },
+      {
+        type = "recipe",
+        name = "large-hydroelectric-turbine-old-to-new",
+        enabled = true,
+        energy_required = 1,
+        ingredients = {{"large-hydroelectric-turbine-old", 1}},
+        result = "large-hydroelectric-turbine"
+      },
+
+
+      {
+        type = "recipe",
+        name = "small-hydroelectric-turbine-new-to-old",
+        enabled = true,
+        energy_required = .5,
+        ingredients = {{"small-hydroelectric-turbine", 1}},
+        result = "small-hydroelectric-turbine-old"
+      },
+      {
+        type = "recipe",
+        name = "large-hydroelectric-turbine-new-to-old",
+        enabled = true,
+        energy_required = 1,
+        ingredients = {{"large-hydroelectric-turbine", 1}},
+        result = "large-hydroelectric-turbine-old"
       }
+
     })
